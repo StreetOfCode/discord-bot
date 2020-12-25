e3b1818f3349d8b97f7901462fc22286cccee4f0
xjahic <jahic.jakub@gmail.com>
2020-12-25 13:30:35 +0100

    Remove unused config variables

--- src/config.py
+++ src/config.py
@@ -6,6 +6,5 @@ load_dotenv()
 
 SERVER_ID = int(os.environ.get("SERVER_ID"))
 TEST_CHANNEL_ID = int(os.environ.get("TEST_CHANNEL_ID"))
-TEST_MEMBER_ID = int(os.environ.get("TEST_MEMBER_ID"))
 DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING")
 TOKEN = os.environ.get("TOKEN")
