64047564c1e0762620d7df4b2eefb8ca29050520
xjahic <jahic.jakub@gmail.com>
2020-12-28 11:07:11 +0100

    Refactor according review

--- src/welcome.py
+++ src/welcome.py
@@ -4,18 +4,6 @@ import db
 from utils import get_server
 from config import ROLE_FOR_NEW_MEMBER, WELCOME_SURVEY_ID
 
-
-async def create_welcome_channel(guild, member):
-    return await guild.create_text_channel(
-        f"vitaj {member.display_name}",
-        overwrites={
-            guild.default_role: discord.PermissionOverwrite(read_messages=False),
-            member: discord.PermissionOverwrite(view_channel=True, add_reactions=True),
-            guild.me: discord.PermissionOverwrite(administrator=True),
-        },
-    )
-
-
 async def send_welcome_message(channel, member, welcome_message):
     embed = discord.Embed(
         title=f"Vitaj {member.display_name}",
@@ -25,9 +13,8 @@ async def send_welcome_message(channel, member, welcome_message):
     await channel.send(embed=embed)
 
 
-async def welcome_member(client, member):
-    guild = get_server(client)
-    # Create channel which can be seen only by this member, bot and admins
+# Create channel which can be seen only by this member, bot and admins
+async def create_welcome_channel(guild, member):
     channel = await guild.create_text_channel(
         f"vitaj {member.display_name}",
         overwrites={
@@ -36,6 +23,12 @@ async def welcome_member(client, member):
             guild.me: discord.PermissionOverwrite(administrator=True),
         },
     )
+    return channel
+
+
+async def welcome_member(client, member):
+    channel = await create_welcome_channel(get_server(client), member)
+
     intro_message = db.get_survey_intro_message(WELCOME_SURVEY_ID)
     await send_welcome_message(channel, member, intro_message)
 
@@ -67,19 +60,19 @@ async def add_reaction_on_survey_answer(client, member, survey_id, question_id, 
     answer_id = db.get_answer_id(question_id, emoji)
 
     # Check if user have already answered this question
-    if (already_answer_id := db.get_answer_of_answered_survey_question(question_id, member.id)) is not None:
+    if (already_answer_id := db.get_answer_of_answered_survey_question_or_none(question_id, member.id)) is not None:
         # Remove answer from db
-        db.remove_user_answer(member.id, question_id, already_answer_id[0])
+        db.remove_user_answer(member.id, question_id, already_answer_id)
         # Remove reaction from answer
-        emoji_to_delete = db.get_emoji_from_survey_answer(already_answer_id[0])
+        emoji_to_delete = db.get_emoji_from_survey_answer(already_answer_id)
         await message.remove_reaction(emoji_to_delete, member)
 
     db.add_answer(member.id, question_id, answer_id)
 
     if db.are_all_survey_questions_answered(member.id):
         # Add receive role if survey contains receive_role_after_finish
-        if (receive_role := db.get_survey_receive_role(survey_id)) is not None:
-            member_role = discord.utils.get(get_server(client).roles, name=receive_role[0])
+        if (receive_role := db.get_survey_receive_role_or_none(survey_id)) is not None:
+            member_role = discord.utils.get(get_server(client).roles, name=receive_role)
             await member.add_roles(member_role)
 
         if survey_id == WELCOME_SURVEY_ID:
