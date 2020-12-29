35a7d782a2ee01374bb7a39c72a8f3d8dd84b8f3
gabrielkerekes <gabriel.kerekes@vacuumlabs.com>
2020-12-29 09:46:46 +0100

    Update config.py to load all vars from env

--- src/config.py
+++ src/config.py
@@ -6,6 +6,5 @@ load_dotenv()
 SERVER_ID = int(os.environ.get("SERVER_ID"))
 DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING")
 TOKEN = os.environ.get("TOKEN")
-
-ROLE_FOR_NEW_MEMBER = "new-member"
-WELCOME_SURVEY_ID = 1
+ROLE_FOR_NEW_MEMBER = os.environ.get("ROLE_FOR_NEW_MEMBER")
+WELCOME_SURVEY_ID = os.environ.get("WELCOME_SURVEY_ID")
