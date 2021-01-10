47cccafc691fbfeef2a467245f7156c5ef971b77
gabrielkerekes <gabriel.kerekes@vacuumlabs.com>
2021-01-10 16:38:31 +0100

    Add _get_first_or_none function to db

--- src/db.py
+++ src/db.py
@@ -5,6 +5,10 @@ from config import DB_CONNECTION_STRING
 db = psycopg2.connect(DB_CONNECTION_STRING)
 
 
+def _get_first_or_none(row):
+    return row[0] if row is not None else None
+
+
 def is_multiple_choice_survey_question(question_id):
     cursor = db.cursor()
     cursor.execute(
@@ -40,7 +44,7 @@ def get_survey_question_order_or_none(question_id):
 
     question = cursor.fetchone()
 
-    return question[0] if question is not None else None
+    return _get_first_or_none(question)
 
 
 def get_next_survey_question(user_id, survey_id):
@@ -107,10 +111,8 @@ def get_user_survey_progress_status_or_none(survey_id, user_id):
         f"SELECT status from user_survey_progress WHERE survey_id={survey_id} AND user_id={user_id}"
     )
     status = cursor.fetchone()
-    if status is not None:
-        return status[0]
-    else:
-        return None
+
+    return _get_first_or_none(status)
 
 
 def clear_all_user_survey_progress(survey_id, user_id):
@@ -171,10 +173,8 @@ def get_survey_receive_role_or_none(survey_id):
         f"SELECT receive_role_after_finish from survey WHERE survey_id={survey_id}"
     )
     receive_role = cursor.fetchone()
-    if receive_role is None:
-        return receive_role
-    else:
-        return receive_role[0]
+
+    return _get_first_or_none(receive_role)
 
 
 def get_survey_question_id_or_none(message_id):
@@ -184,10 +184,8 @@ def get_survey_question_id_or_none(message_id):
     )
 
     question_id = cursor.fetchone()
-    if question_id is None:
-        return question_id
-    else:
-        return question_id[0]
+
+    return _get_first_or_none(question_id)
 
 
 def get_survey_id_from_user_survey_progress_or_none(user_id, channel_id):
@@ -196,10 +194,8 @@ def get_survey_id_from_user_survey_progress_or_none(user_id, channel_id):
         f"SELECT survey_id FROM user_survey_progress WHERE user_id={user_id} AND channel_id={channel_id}"
     )
     survey_id = cursor.fetchone()
-    if survey_id is None:
-        return survey_id
-    else:
-        return survey_id[0]
+
+    return _get_first_or_none(survey_id)
 
 
 def is_survey_progress_finished(survey_id, user_id):
@@ -208,7 +204,8 @@ def is_survey_progress_finished(survey_id, user_id):
         f"SELECT status FROM user_survey_progress WHERE survey_id={survey_id} AND user_id={user_id}"
     )
     status = cursor.fetchone()
-    return status[0] == "FINISHED" if status is not None else False
+
+    return _get_first_or_none(status) == "FINISHED"
 
 
 def get_answer_id(survey_question_id, emoji):
@@ -227,10 +224,8 @@ def get_answer_of_answered_survey_question_or_none(survey_question_id, user_id):
     )
 
     answer_id = cursor.fetchone()
-    if answer_id is None:
-        return answer_id
-    else:
-        return answer_id[0]
+
+    return _get_first_or_none(answer_id)
 
 
 def remove_user_answer(user_id, survey_question_id, survey_answer_id):
@@ -257,6 +252,7 @@ def is_last_question(survey_id, question_id):
     )
 
     last_question = cursor.fetchone()
+
     return last_question[0] == question_id if last_question is not None else False
 
 
