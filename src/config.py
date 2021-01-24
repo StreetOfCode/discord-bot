ee142545cfa30fc140384c22423516ef2df24979
Kobu <thekobu@gmail.com>
2021-01-24 09:57:51 +0100

    Wrap admin_commands in a cog

--- src/config.py
+++ src/config.py
@@ -7,8 +7,8 @@ load_dotenv()
 SERVER_ID = int(os.environ.get("SERVER_ID"))
 DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING")
 TOKEN = os.environ.get("TOKEN")
-ADMIN_ROLE = os.environ.get("ADMIN_ROLE")
 WELCOME_SURVEY_ID = int(os.environ.get("WELCOME_SURVEY_ID"))
+ADMIN_ROLE_ID = int(os.environ.get("ADMIN_ROLE_ID"))
 PING_UNANSWERED_SURVEY_OLDER_THAN = os.environ.get("PING_UNANSWERED_SURVEY_OLDER_THAN")
 DELETE_FINISHED_SURVEYS_OLDER_THAN = os.environ.get(
     "DELETE_FINISHED_SURVEYS_OLDER_THAN"
